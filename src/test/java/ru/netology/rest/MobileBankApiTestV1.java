4debc13c238068c4aa3a01b7230106cdcc3046ad
Finikys <ForOnceFinikys@gmail.com>
2023-03-02 09:23:00 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,34 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+
+
+    }
+    @Test
+    void jsonSchemaValidator(){
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+}
